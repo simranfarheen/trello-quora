77c6e8fd3513e4956ff42c40b1fe1efb18a3b96b
Simran Farheen <simranf996@gmail.com>
2021-01-25 03:17:55 +0530

    Removed hardcoding for salt and added method for user signin

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,18 +1,25 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.AuthenticationService;
 import com.upgrad.quora.service.business.UserService;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +28,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
 
 
     //TODO: Check if username exists in database, if yes throw proper exception
@@ -36,9 +46,10 @@ public class UserController {
         userEntity.setFirstName(signupUserRequest.getFirstName());
         userEntity.setLastName(signupUserRequest.getLastName());
         userEntity.setUsername(signupUserRequest.getUserName());
+        //String encodePassword = Base64.getEncoder().encodeToString(signupUserRequest.getPassword().getBytes());
+        userEntity.setPassword(signupUserRequest.getPassword());
         userEntity.setEmail(signupUserRequest.getEmailAddress());
         userEntity.setContactNumber(signupUserRequest.getContactNumber());
-        userEntity.setSalt("1234abc");
         userEntity.setAboutMe(signupUserRequest.getAboutMe());
         userEntity.setCountry(signupUserRequest.getCountry());
         userEntity.setDob(signupUserRequest.getDob());
@@ -47,6 +58,24 @@ public class UserController {
         final UserEntity createdUserEntity = userService.signup(userEntity);
         SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("User Successfully Registered");
         return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+
+
+        byte[] decode = Base64.getMimeDecoder().decode(authorization.split("Basic ")[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+
+        UserAuthTokenEntity userAuthToken = authenticationService.authenticate(decodedArray[0], decodedArray[1]);
+        UserEntity user = userAuthToken.getUser();
+
+        SigninResponse authorizedUserResponse = new SigninResponse().id(user.getUuid()).message("Signed in Successfully");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+        return new ResponseEntity<SigninResponse>(authorizedUserResponse, headers, HttpStatus.OK);
     }
 
 

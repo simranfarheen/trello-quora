65996abd3b3eddb553cd58789ce55ed7a4544bf0
Simran Farheen <simranf996@gmail.com>
2021-01-25 00:38:17 +0530

    Added code to allow users to signup using the API endpoint /user/signup

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,4 +1,53 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.UserService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    //TODO: Check if username exists in database, if yes throw proper exception
+    //TODO: If email exists throw appropriate exception
+    //TODO: If user details are proper, return uuid and message "User successfully registered" in JSON response with HTTP status
+    //TODO: Before saving password, ensure to encrypt it using 'PasswordCryptographyProvider' class
+    //TODO: Role will be nonadmin if user signs up using this endpoint
+
+    @RequestMapping(method = RequestMethod.POST, path ="/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
+        final UserEntity userEntity = new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setUsername(signupUserRequest.getUserName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setContactNumber(signupUserRequest.getContactNumber());
+        userEntity.setSalt("1234abc");
+        userEntity.setAboutMe(signupUserRequest.getAboutMe());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setRole("nonadmin");
+
+        final UserEntity createdUserEntity = userService.signup(userEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("User Successfully Registered");
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
+    }
+
+
 }
--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -1,4 +1,206 @@
 package com.upgrad.quora.service.entity;
 
-public class UserEntity {
+import org.apache.commons.lang3.builder.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@NamedQueries(
+        {
+                @NamedQuery(name = "userByUuid", query = "select u from UserEntity u where u.uuid = :uuid"),
+                @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email =:email"),
+                @NamedQuery(name = "userByUsername", query = "select u from UserEntity u where u.username =:username")
+        }
+)
+
+
+public class UserEntity implements Serializable{
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "firstname")
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
+
+    @Column(name = "lastname")
+    @NotNull
+    @Size(max = 30)
+    private String lastName;
+
+    @Column(name = "username")
+    @NotNull
+    @Size(max = 30)
+    private String username;
+
+    @Column(name = "email")
+    @NotNull
+    @Size(max = 50)
+    private String email;
+
+    @ToStringExclude
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "salt")
+    @NotNull
+    @Size(max = 200)
+    @ToStringExclude
+    private String salt;
+
+    @Column(name = "country")
+    @Size(max = 30)
+    private String country;
+
+    @Column(name = "aboutme")
+    @Size(max = 50)
+    private String aboutMe;
+
+    @Column(name = "dob")
+    @Size(max = 30)
+    private String dob;
+
+    @JoinColumn(name = "role")
+    @Size(max = 30)
+    private String role;
+
+    @Column(name = "contactnumber")
+    @NotNull
+    @Size(max = 30)
+    private String contactNumber;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+
 }

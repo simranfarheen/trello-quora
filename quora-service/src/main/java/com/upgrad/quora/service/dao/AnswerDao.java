abbaf02e6603cb501fb7f9098097a256edd6d444
Simran Farheen <simranf996@gmail.com>
2021-01-25 08:18:53 +0530

    Added DAO logic and methods for answers in code

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -1,4 +1,39 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
 public class AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public AnswerEntity addAnswer(AnswerEntity answerEntity) {
+        entityManager.persist(answerEntity);
+        return answerEntity;
+    }
+
+    public AnswerEntity editAnswer(final AnswerEntity answerEntity) {
+        entityManager.merge(answerEntity);
+        return answerEntity;
+    }
+
+    public AnswerEntity deleteQuestion(final AnswerEntity answer) {
+        entityManager.remove(answer);
+        return answer;
+    }
+
+    public List<AnswerEntity> getAllAnswersByQuestionId(final String answerId) {
+        try {
+            return entityManager.createNamedQuery("answersByUuid", AnswerEntity.class).setParameter("uuid", answerId).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
 }

d2eed2fb8dac5683fce8a94e9ed2f3a3ffb864a5
harsh-tech-source <harshmalik7777@gmail.com>
2021-01-27 20:35:49 +0530

    Bug fixed for getUser method

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
@@ -45,17 +45,18 @@ public class UserService {
     public UserEntity getUser(final String userUUID, final String accessToken) throws AuthenticationFailedException, UserNotFoundException {
 
         UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthToken(accessToken);
-        UserEntity userEntity = userDao.getUser(userUUID);
-
-        if(userEntity == null)
-            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
-
         if (userAuthTokenEntity == null) {
             throw new AuthenticationFailedException("ATHR-001", "User has not signed in");
         }
 
         if(userAuthTokenEntity.getLogoutAt()!=null)
             throw new AuthenticationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
+
+        UserEntity userEntity = userDao.getUser(userUUID);
+
+        if(userEntity == null)
+            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
+
 
 
         return userEntity;
